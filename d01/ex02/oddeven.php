505d4ad950c65d60781b57961f18b527be137e3c
Veneranda <hveneranda@yahoo.com>
2020-02-25 22:10:28 -0800

    odd

--- d01/ex02/oddeven.php
+++ d01/ex02/oddeven.php
@@ -1,5 +1,22 @@
 #!/usr/bin/php
 <?php
 echo ("Enter a number: $n")
-
+$open = fopen('php://stdin', 'r');
+	while (!feof($open))
+	{
+		echo "Enter a number: ";
+		$input = fgets($open);
+		$input = str_replace("\n", "", $input);
+		if (is_numeric($input))
+		{
+			if ($input % 2 == 0)
+				echo "The number" . " $input" . " is even\n";
+			else
+				echo "The number" . " $input" . " is odd\n";
+		}
+		else
+			echo "'". "$input" . "'" . " is not a number\n";
+	}
+	fclose($open);
+	
 ?>
--- d01/ex03/ft_split.php
+++ d01/ex03/ft_split.php
@@ -1,10 +1,12 @@
 #!/usr/bin/php
 <?php
     function ft_split($str){
+        $str = preg_replace('/\s\s+/', ' ', $str);
         $tmp = explode(" ", $str);
+        sort($tmp, SORT_REGULAR);
         return($tmp);
     }
 
-    print_r(ft_split("Hello hi ji"));
+    print_r(ft_split("Hello   World AAA"));
 ?>
 
